e7b937cfe5301c658366efaaf0d4792f7e30e9ad
glowbowl <lyashko2@gmail.com>
2018-04-30 04:54:55 +0300

    ''

---
+++ bot.py
@@ -0,0 +1,38 @@
+import requests
+from time import sleep
+
+url = "https://api.telegram.org/bot584253782:AAGNnxIbuHCCXkfL6UGHDBuikDfon093mBI/"
+
+def main():
+    update_id = last_update(get_updates_json(url))['update_id']
+    while True:
+        if update_id == last_update(get_updates_json(url))['update_id']:
+            send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
+            update_id += 1
+        sleep(1)
+
+def get_updates_json(request):
+    params = {'timeout': 100, 'offset': None}
+    response = requests.get(request + 'getUpdates')
+    return response.json()
+
+def last_update(data):
+    result = data['result']
+    total_updates = len(result) - 1
+    return result[total_updates]
+
+def get_chat_id(update):
+    chat_id = update['message']['chat']['id']
+    return chat_id
+
+def send_mess(chat, text):
+    params = {'chat_id': chat, 'text' : text}
+    response = requests.post(url + 'sendMessage', data=params)
+    return response
+
+if __name__ == '__main__':
+    main()
+
+chat_id = get_chat_id(last_update(get_updates_json(url)))
+
+send_mess(chat_id, 'Suck it')
