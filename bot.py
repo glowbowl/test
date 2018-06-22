8a8791cec98f3305edfb3fe68a0e174ea3e3d3a1
glowbowl <lyashko2@gmail.com>
2018-06-23 00:20:01 +0300

    'update'

--- bot.py
+++ bot.py
@@ -12,8 +12,7 @@ TOKEN = '584253782:AAGNnxIbuHCCXkfL6UGHDBuikDfon093mBI'
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
-PORT = int(os.environ.get('PORT', '8443'))
-updater = Updater(TOKEN)
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -24,6 +23,14 @@ def start(message):
 def echo_message(message):
     bot.reply_to(message, message.text)
 
+def send_mess(chat, text):
+    params = {'chat_id': chat, 'text' : text}
+    response = requests.post(url + 'sendMessage', data=params)
+    return response
+	
+def get_chat_id(update):
+    chat_id = update['message']['chat']['id']
+    return chat_id
 
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
@@ -37,11 +44,10 @@ def webhook():
     bot.set_webhook(url='https://rocky-sierra-97001.herokuapp.com/' + TOKEN)
     return "!", 200
 
-updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=TOKEN)
-updater.bot.set_webhook("https://rocky-sierra-97001.herokuapp.com/" + TOKEN)
-updater.idle()
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+	start();
+	
+chat_id = get_chat_id(last_update(get_updates_json(url)))
+send_mess(chat_id, 'Suck it')
