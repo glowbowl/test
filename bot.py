3c751d76c84e8cbdaff29751d14e3762f67f0fe1
glowbowl <lyashko2@gmail.com>
2018-06-23 23:23:15 +0300

    ''

--- bot.py
+++ bot.py
@@ -75,6 +75,7 @@ def webhook():
     bot.set_webhook(url='https://rocky-sierra-97001.herokuapp.com/' + TOKEN)
     return "!", 200
 
+main()
+	
 if __name__ == "__main__":
-	main()
-    #server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
