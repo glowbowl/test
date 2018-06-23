54a74a09296d2e8742b03442da2a376bb40dc893
glowbowl <lyashko2@gmail.com>
2018-06-23 23:05:05 +0300

    ''

--- bot.py
+++ bot.py
@@ -39,21 +39,15 @@ def get_last_chat_id_and_text(updates):
 def send_message(text, chat_id):
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
-    
-#@server.route('/' + TOKEN, methods=['POST'])
-#def getMessage():
-#    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
- #   return "!", 200
-
 
 @server.route("/")
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url='https://rocky-sierra-97001.herokuapp.com/' + TOKEN)
     return "!", 200
-
-text, chat = get_last_chat_id_and_text(get_updates())
-send_message(text, chat)
+while(True):
+	text, chat = get_last_chat_id_and_text(get_updates())
+	send_message(text, chat)
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
